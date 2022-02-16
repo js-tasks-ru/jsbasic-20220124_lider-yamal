2ca21b0543c808a3fab663492331eb712f480991
Max Pekur <lider.yamal@mail.ru>
2022-02-16 11:06:09 +0500

    Solved 4-module, 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+  const carouselInner = document.querySelector(".carousel__inner");
+  const slideCount = carouselInner.children.length;
+  let currentSlide = 1;
+  let pos = 0;
+
+  checkBtn();
+
+  arrowLeft.addEventListener("click", moveLeft);
+  arrowRight.addEventListener("click", moveRigth);
+
+  function moveLeft() {
+    pos -= carouselInner.offsetWidth;
+    ++currentSlide;
+    setPos();
+  }
+
+  function moveRigth() {
+    pos += carouselInner.offsetWidth;
+    --currentSlide;
+    setPos();
+  }
+
+  function setPos() {
+    carouselInner.style.transform = `translateX(${pos}px)`;
+    checkBtn();
+  }
+
+  function checkBtn() {
+    currentSlide === 1 ? arrowRight.style.display = "none" : arrowRight.style.display = "";
+    currentSlide === slideCount ? arrowLeft.style.display = "none" : arrowLeft.style.display = "";
+    
+  }
 }
